df27cf0241b3b83f018e9a5e86ae5857eea67710
ismet0950 <mamedovisi57@gmail.com>
2026-03-02 21:24:05 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { Pool } = require("pg"); I
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Database connection
+const pool = new Pool({
+    user: process.env.DB_USER || "postgres",
+    host: process.env.DB_HOST || "database",
+    database: process.env.DB_NAME || "postgres",
+    password: process.env.DB_PASS || "password",
+    port: 5432,
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello from Express Backend!");
+});
+
+app.get("/db", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW( )");
+        res.json({ time: result.rows[0] });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
